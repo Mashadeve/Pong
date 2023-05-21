0a7c7e03f59fa64f4fd599acc7c64848e9fee313
zzokku <93818329+zzokku@users.noreply.github.com>
2023-05-21 14:00:21 +0300

    Sliders moving and balls colliding

--- main.py
+++ main.py
@@ -2,30 +2,45 @@ import pygame as pg
 import sys
 from settings import *
 from map import Map
+from slider import *
+
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
-        self.map = Map(self.screen)
-
+        self.slider_left = Slider(LEFT_X, LEFT_Y)
+        self.slider_right = Slider(RIGHT_X, RIGHT_Y)
+        self.map = Map(self.screen, self.slider_left, self.slider_right)
 
     def update(self):
         pg.display.flip()
         self.clock.tick(FPS)
-        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+        pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
 
     def draw(self):
-        self.screen.fill('black')
-        self.map.draw_ball()
-        self.map.draw_sliders()
+        self.screen.fill("black")
+        self.map.draw_all(self.slider_left, self.slider_right)
 
     def check_events(self):
+        key_input = pg.key.get_pressed()
+        pg.key.set_repeat()
         for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+            if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+   
+        key_input = pg.key.get_pressed()
+        if key_input[pg.K_UP]:
+            self.slider_left.move(-1)
+        if key_input[pg.K_DOWN]:
+            self.slider_left.move(1)
+        if key_input[pg.K_w]:
+            self.slider_right.move(-1)
+        if key_input[pg.K_s]:
+            self.slider_right.move(1)
+
 
     def run(self):
         while True:
@@ -34,10 +49,6 @@ class Game:
             self.draw()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     game = Game()
     game.run()
-
-
-
-
--- slider.py
+++ slider.py
@@ -1,10 +1,12 @@
-import pygame as pg
 from settings import *
 
+
 class Slider:
-
-    def __init__(self, position):
+    def __init__(self, x, y):
         self.slider_size = SLIDER_SIZE
-        self.slider_pos = position
+        self.slider_pos_x = x
+        self.slider_pos_y = y
 
-
+    def move(self, direction):
+        self.slider_pos_y += direction * 5
+        
