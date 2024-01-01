aa7d5c74f50fc09f4559e577bab7a87151b4635d
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2024-01-01 15:56:19 +0200

    asdasd

--- map.py
+++ map.py
@@ -2,18 +2,13 @@ import pygame as pg
 from settings import *
 from slider import *
 from ball import Ball
-import random
 
 class Map:
-
-    
 
     def __init__(self, screen, slider_left, slider_right):
         self.screen = screen
         self.slider_left = slider_left
         self.slider_right = slider_right
-        self.left_score = 0
-        self.right_score = 0
         self.ball = Ball()
        
 
@@ -24,20 +19,6 @@ class Map:
         self.ball.move()
         self.ball.collisions((l_x, l_y), (r_x, r_y))
 
-        if self.ball.check_position() == "LEFT":
-            self.right_score += 1
-            self.ball.x = WIDTH/2
-            self.ball.y = HEIGHT/2
-            self.ball.vx = [1, -1][int(round(random.random()))] * 250
-            self.ball.vy = [1, -1][int(round(random.random()))] * 100
-            
-        if self.ball.check_position() == "RIGHT":
-            self.left_score += 1
-            self.ball.x = WIDTH/2
-            self.ball.y = HEIGHT/2
-            self.ball.vx = [1, -1][int(round(random.random()))] * 250
-            self.ball.vy = [1, -1][int(round(random.random()))] * 100
-
     def draw_sliders(self, slider_left, slider_right):
         l_x, l_y = slider_left.slider_pos_x, slider_left.slider_pos_y
         r_x, r_y = slider_right.slider_pos_x, slider_right.slider_pos_y
@@ -47,15 +28,3 @@ class Map:
     def draw_all(self, slider_left, slider_right):
         self.draw_ball(slider_left, slider_right)
         self.draw_sliders(slider_left, slider_right)
-        self.draw_score()
-
-    def draw_score(self):
-        font = pg.font.Font('freesansbold.ttf', 32)
-        text = font.render(f'{self.left_score} - {self.right_score}', True, WHITE, BLACK)
-        textRect = text.get_rect()
-        textRect.center = (WIDTH // 2, 50)
-        self.screen.blit(text, textRect)
-
-
-
-    
